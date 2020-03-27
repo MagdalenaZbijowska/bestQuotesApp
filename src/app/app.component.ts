b3cd3f7e8c6810b4d69bfa225a7b37aeb6770d17
user <aaa@wp.pl>
2020-03-27 17:24:19 +0100

    my third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Quotation } from './models/quotation';
+import { QUOTES } from './models/database';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bestQuotesApp';
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {
+    author: '', sentence: '', votes: 0
+  };
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = { author: '', sentence: '', votes: 0 };
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
 }
