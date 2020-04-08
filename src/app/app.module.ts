4ca7f91539987b116f6ee76594cd2bd693e7fc5c
user <aaa@wp.pl>
2020-04-08 21:00:56 +0200

    ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RankingComponent,
     NavbarComponent,
-    FormComponent
+    FormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
